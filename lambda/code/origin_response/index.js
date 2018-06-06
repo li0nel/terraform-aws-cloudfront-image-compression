af5157f700b43a9bff7195a24e3bd5c98a6ba656
Lionel Martin <corsaaaire@gmail.com>
2018-06-06 11:54:49 +0100

    preventing lambda to continue after callback

--- lambda/code/origin_response/index.js
+++ lambda/code/origin_response/index.js
@@ -10,80 +10,110 @@ exports.handler = (event, context, callback) => {
     const request = event.Records[0].cf.request
     const origin = url.parse(request.uri.substring(1))
 
-    console.log(origin)
-
-    const getFile = origin.protocol === 'https:' ?
-        https.get :
-        http.get
-
-    const options = querystring.parse(request.querystring)
-    const maxSize = 2000
-    const width = Math.min(options.width || maxSize, maxSize)
-    const height = Math.min(options.height || maxSize, maxSize)
-
-    console.log(width)
-    console.log(height)
-
-    // make sure input values are numbers
-    if (Number.isNaN(width) || Number.isNaN(height) || origin.protocol === null) {
-        console.log('Invalid input')
+    if (request.uri === '/favicon.ico') {
         callback(null, {
-            status: '400',
-            statusDescription: 'Invalid input'
+            bodyEncoding: 'base64',
+            body: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=',
+            headers: {
+                'content-type': [{
+                    key: 'Content-Type',
+                    value: 'image/x-icon'
+                }]
+            },
+            status: '200',
+            statusDescription: 'OK'
         })
-    }
+    } else {
+        const getFile = origin.protocol === 'https:' ?
+            https.get :
+            http.get
 
-    // whitelisting of domains
-    if (domains.length && domains.indexOf(origin.host) === -1) {
-        console.log('Invalid domain')
-        callback(null, {
-            status: '403',
-            statusDescription: 'Invalid domain'
-        })
-    }
+        const options = querystring.parse(request.querystring)
+        const maxSize = 2000
+        const width = Math.min(options.width || maxSize, maxSize)
+        const height = Math.min(options.height || maxSize, maxSize)
 
-    // download the file from the origin server
-    getFile(origin.href, (res) => {
-        const statusCode = res.statusCode
+        console.log(width)
+        console.log(height)
 
-        if (statusCode === 200) {
-            var data = []
+        // make sure input values are numbers
+        if (Number.isNaN(width) || Number.isNaN(height) || origin.protocol === null) {
+            callback(null, {
+                status: '400',
+                statusDescription: 'Invalid input'
+            })
+        } else {
+            // whitelisting of domains
+            if (domains.length && domains.indexOf(origin.host) === -1) {
+                console.log('Invalid domain')
+                callback(null, {
+                    status: '403',
+                    statusDescription: 'Invalid domain'
+                })
+            } else {
+                // download the file from the origin server
+                getFile(origin.href, (res) => {
+                    const statusCode = res.statusCode
 
-            res.on('data', function(chunk) {
-                data.push(chunk)
-            }).on('end', function() {
-                var buffer = Buffer.concat(data)
+                    if(statusCode === 200) {
+                    var data = []
 
-                try {
-                    Sharp(buffer)
-                        .resize(width, height)
-                        .jpeg({
-                            quality: 80,
-                            chromaSubsampling: '4:4:4',
-                            force: true
-                        })
-                        .max()
-                        .toBuffer()
-                        .then( (_data) =>  {
-                            console.log('Sharp finished')
-                            callback(null, {
-                                bodyEncoding: 'base64',
-                                body: new Buffer(_data, 'binary').toString('base64'),
-                                headers: {
-                                    'cache-control': [{
-                                        key: 'Cache-Control',
-                                        value: 'max-age=100'
-                                    }],
-                                    'content-type': [{
-                                        key: 'Content-Type',
-                                        value: 'image/jpeg'
-                                    }]
-                                },
-                                status: '200',
-                                statusDescription: 'OK'
-                            })
-                        }).catch( (err) =>  {
-                            console.log(err)
+                    res.on('data', function (chunk) {
+                        data.push(chunk)
+                    }).on('end', function () {
+                        var buffer = Buffer.concat(data)
+
+                        try {
+                            Sharp(buffer)
+                                .resize(width, height)
+                                .jpeg({
+                                    quality: 80,
+                                    chromaSubsampling: '4:4:4',
+                                    force: true
+                                })
+                                .max()
+                                .toBuffer()
+                                .then((_data) => {
+                                    console.log('Sharp finished')
+                                    callback(null, {
+                                        bodyEncoding: 'base64',
+                                        body: new Buffer(_data, 'binary').toString('base64'),
+                                        headers: {
+                                            'cache-control': [{
+                                                key: 'Cache-Control',
+                                                value: 'max-age=100'
+                                            }],
+                                            'content-type': [{
+                                                key: 'Content-Type',
+                                                value: 'image/jpeg'
+                                            }]
+                                        },
+                                        status: '200',
+                                        statusDescription: 'OK'
+                                    })
+                                }).catch((err) => {
+                                        console.log(err)
+                                    callback(null, {
+                                        status: '302',
+                                        statusDescription: 'Found',
+                                        headers: {
+                                            location: [{
+                                                key: 'Location',
+                                                value: origin.href,
+                                            }],
+                                        },
+                                    })
+                                })
+                            } catch (e) {
+                                console.log('Sharp error')
+                                console.log(e.stderr)
+                                callback(null, {
+                                    status: '500',
+                                    statusDescription: 'Error resizing image'
+                                })
+                            }
+                        }).on('error', (e) => {
+                            console.log(e)
                             callback(null, {
                                 status: '302',
                                 statusDescription: 'Found',
@@ -91,39 +121,21 @@ exports.handler = (event, context, callback) => {
                                     location: [{
                                         key: 'Location',
                                         value: origin.href,
-                                    }],
-                                },
+                                    }]
+                                }
                             })
                         })
-                } catch(e) {
-                    console.log('Sharp error')
-                    console.log(e.stderr)
-                    callback(null, {
-                        status: '500',
-                        statusDescription: 'Error resizing image'
-                    })
-                }
-            }).on('error', (e) => {
-                console.log(e)
-                callback(null, {
-                    status: '302',
-                    statusDescription: 'Found',
-                    headers: {
-                        location: [{
-                            key: 'Location',
-                            value: origin.href,
-                        }],
-                    },
+                    } else {
+                        // grap the status code from the origin request
+                        // and return to the viewer
+                        console.log('statusCode: ', statusCode)
+                        callback(null, {
+                            status: statusCode.toString(),
+                            headers: originHeaders
+                        })
+                    }
                 })
-            })
-        } else {
-            // grap the status code from the origin request
-            // and return to the viewer
-            console.log('statusCode: ', statusCode)
-            callback(null, {
-                status: statusCode.toString(),
-                headers: originHeaders
-            })
+            }
         }
-    })
+    }
 }
