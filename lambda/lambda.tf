41cf67460085abdf6b898185eb302939362382b5
Lionel Martin <corsaaaire@gmail.com>
2018-07-13 11:14:58 +0100

    add stack_name to iam_role name

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -18,7 +18,7 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda_at_edge"
+  name = "iam_for_lambda_at_edge-${var.stack_name}"
 
   assume_role_policy = <<EOF
 {
